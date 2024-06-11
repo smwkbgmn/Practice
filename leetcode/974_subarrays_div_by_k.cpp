48b1d669a64dfe138b0ee689a3247e2eac9771b6
Donghyun Kim <donghyu2@cx1r5s4.42seoul.kr>
2024-06-11 12:49:43 +0900

    1

--- leetcode/974_subarrays_div_by_k.cpp
+++ leetcode/974_subarrays_div_by_k.cpp
@@ -31,33 +31,124 @@ class Solution {
 		int subarraysDivByK(std::vector<int>&, int);
 };
 
-/* 2 */
+#include <iostream>
+
+/* 3 */
 int Solution::subarraysDivByK(std::vector<int>& nums, int k) {
+	if (nums.size() == 1) {
+		if (nums.front() % k == 0) return 1;
+		else return 0;
+	}
+
 	int rst = 0;
 
-	auto it = nums.begin();
-
-	int crit = *it % k;
-	if (crit == 0) ++rst;
-
-	int subchain = 0;
-	for (auto it_next = it + 1; it_next != nums.end(); ++it_next) {
-		*it += *it_next;
-
-		int mod = *it % k;
-		if (mod == 0) {
-			int fact = subchain--;
-			while (subchain > 0) fact *= subchain--;
-			rst += fact + 1;
-			crit = 0;
+	int chain = 0;
+	for (auto it = nums.begin(); it != nums.end(); ) {
+		std::cout << "it pointing element " << *it << "\n";
+		if (*it % k == 0) {
+			++chain;
+			++it;
 		}
-		else if (mod == crit || mod == crit - k) {
-			*it = crit;
-			++subchain;
+		else {
+			int sub = 0;
+			int submod = 0;
+			
+			auto it_sub = it + 1;
+			for (; it_sub != nums.end(); ++it_sub) {
+				*it = (*it + *it_sub) % k;
+
+				if (*it == 0) {
+					++chain;
+					break;
+				}
+				else if (*it_sub % k == 0) ++sub;
+				else {
+					submod = (submod + *it_sub) % k;
+					if (submod == 0) ++sub;
+				}
+			}
+			
+			if (sub) {
+				int facto = sub--;
+				while (sub) facto *= sub--;
+				rst += facto;
+			}
+
+			it = it_sub;
 		}
 	}
+
+	if (chain) {
+		int facto = chain--;
+		while (chain) facto *= chain--;
+		rst += facto;
+	}
+
 	return rst;
 }
+
+/* 2 */
+// int Solution::subarraysDivByK(std::vector<int>& nums, int k) {
+// 	int rst = 0;
+
+// 	int chain = 0;
+// 	for (auto it = nums.begin(); it != nums.end();) {
+// 		if (*it % k == 0) {
+// 			++chain;
+// 			++it;
+// 		}
+
+// 		else {
+// 			int crit = *it % k;
+// 			int sub = 0;
+
+// 			auto it_sub = it + 1;
+// 			for (; it_sub != nums.end(); ++it_sub) {
+// 				*it = (*it + *it_sub) % k;
+// 				// std::cout << "now *it: " << *it << "\n";
+
+// 				if (*it % k == 0) break;
+// 				else if (*it_sub % k == 0 || *it == crit || *it == crit + (k * (crit < 0? 1 : -1))) {
+// 					// std::cout << "subary added " << *it << ", " << crit << "\n";
+// 					++sub;
+// 				}
+// 			}
+			
+// 			// std::cout << "subary done with " << sub << "\n";
+
+// 			int fact = 0;
+
+// 			if (sub) {
+// 				fact = sub--;
+// 				while (sub) fact *= sub--;
+// 			}
+
+// 			rst += fact;
+
+// 			if (*it % k != 0) {
+// 				int fact = 0;
+
+// 				if (chain) {
+// 					fact = chain--;
+// 					while (chain) fact *= chain--;
+// 				}
+
+// 				rst += fact;
+// 			}
+// 			else ++chain;
+
+// 			it = it_sub;
+// 		}
+// 	} 
+
+// 	if (chain) {
+// 		int fact = chain--;
+// 		while (chain) fact *= chain--;
+// 		rst += fact;
+// 	}
+
+// 	return rst;
+// }
 
 /* 1 */
 // int Solution::subarraysDivByK(std::vector<int>& nums, int k) {
@@ -76,3 +167,23 @@ int Solution::subarraysDivByK(std::vector<int>& nums, int k) {
 	
 // 	return rst;
 // }
+
+int main(void) {
+	Solution test;
+	std::vector<int> vec;
+
+	vec.push_back(7);
+	vec.push_back(-5);
+	vec.push_back(5);
+	vec.push_back(-8);
+	vec.push_back(-6);
+	vec.push_back(6);
+	vec.push_back(-4);
+	vec.push_back(7);
+	vec.push_back(-8);
+	vec.push_back(-7);
+
+	std::cout << test.subarraysDivByK(vec, 7) << std::endl;
+	
+	return 0;
+}
